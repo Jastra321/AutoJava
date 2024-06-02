729e5521655c348a928f48bb2315e728535dff1f
Jastra321 <k@k.k>
2024-06-02 21:32:15 +0300

    DZ_10  part 2 of 12. make with HashMap

---
+++ DZ_10/src/main/java/org/autojava/Solution217.java
@@ -0,0 +1,27 @@
+package org.autojava;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Solution217 {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap.Entry<Integer, Long> maxEntry = Arrays.stream(nums).boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(
+                        (e1, e2) ->
+                                e1.getValue() == e2.getValue()
+                                        ?
+                                        Long.compare(e1.getKey(), e2.getKey())
+                                        :
+                                        Long.compare(e1.getValue(), e2.getValue())
+                )
+                .get();
+        boolean returnA = false;
+        if (maxEntry.getValue() > 1L) returnA = true;
+        return returnA;
+    }
+}
